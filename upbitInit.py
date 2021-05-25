5ea7e622147536f645bef490fe0e91c5fce77f23
seonmi0812 <whangsm@gmail.com>
2021-05-25 21:09:01 +0900

    test

---
+++ upbitInit.py
@@ -0,0 +1,20 @@
+import pyupbit
+
+#
+# print(pyupbit.get_tickers())
+
+# class test:
+#     def init1(first, secoend, third):
+        
+# result =2
+# def add1(test):
+#     global result 
+#     result+=2
+#     test += 1
+#     return test
+
+# print(add1(10))   
+# print(result)  
+
+bitcoin='abcde'
+print(bitcoin[2])
